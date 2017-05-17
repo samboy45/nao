d8e42481c7b7c4e65f1580f91103350f10aaa7a3
Yann CLEMENT <biduletruck@gmail.com>
2017-05-17 14:49:13 +0200

    modification security.yml, appKernel et User entity

--- src/NAO/UserBundle/Entity/User.php
+++ src/NAO/UserBundle/Entity/User.php
@@ -2,34 +2,25 @@
 
 namespace NAO\UserBundle\Entity;
 
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * User
- *
- * @ORM\Table(name="user")
- * @ORM\Entity(repositoryClass="NAO\UserBundle\Repository\UserRepository")
+ * @ORM\Entity
+ * @ORM\Table(name="utilisateurs")
  */
-class User
+class User extends BaseUser
 {
     /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer")
      * @ORM\Id
+     * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
 
-
-    /**
-     * Get id
-     *
-     * @return int
-     */
-    public function getId()
+    public function __construct()
     {
-        return $this->id;
+        parent::__construct();
+        // your own logic
     }
 }
-
