008f6513e79c4803d96c71dc54f22adec22f2cd0
yann clement <clementyann@free.fr>
2017-05-26 17:18:24 +0200

    mise à jour des vues et des routes annotations vers yml

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,14 +8,22 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
+
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    public function mentionsLegalesAction()
+    {
+        return $this->render("mentions/mentions_legales.html.twig");
+    }
+
+    public function conditionsGeneralesUtlisationAction()
+    {
+        return $this->render("mentions/cgu.html.twig");
     }
 }
