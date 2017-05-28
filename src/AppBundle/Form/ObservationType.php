66fad0ab9724c645c623f225f11223a7e5aaa8cc
samboy45 <170145@supinfo.com>
2017-05-28 21:39:38 +0200

    formulaire observation ok

--- src/AppBundle/Form/ObservationType.php
+++ src/AppBundle/Form/ObservationType.php
@@ -2,9 +2,15 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\Espece;
+use AppBundle\Entity\Famille;
+use AppBundle\Entity\Ordre;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ObservationType extends AbstractType
@@ -21,10 +27,73 @@ class ObservationType extends AbstractType
             ->add('image')
             ->add('ordre', EntityType::class, array(
                 'class' => 'AppBundle\Entity\Ordre',
-                'placeholder' => 'Selectionner l\'ordre',
+                'placeholder' => 'Selectionnez l\'ordre',
                 'mapped' => false,
                 'required' => false
             ));
+        $builder->get('ordre')->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) {
+                $form = $event->getForm();
+                $this->addFamilleField($form->getParent(), $form->getData());
+            }
+        );
+        $builder->addEventListener(
+            FormEvents::POST_SET_DATA,
+            function (FormEvent $event) {
+                $data = $event->getData();
+                /* @var $espece Espece */
+                $espece = $data->getEspece();
+                $form = $event->getForm();
+                if ($espece) {
+                    $famille = $espece->getFamille();
+                    $ordre = $famille->getOrdre();
+                    $this->addFamilleField($form, $ordre);
+                    $this->addEspeceField($form, $famille);
+                    $form->get('ordre')->setData($ordre);
+                    $form->get('famille')->setData($famille);
+                } else {
+                    $this->addFamilleField($form, null);
+                    $this->addEspeceField($form, null);
+                }
+            }
+        );
+    }
+
+
+
+    private function  addFamilleField(FormInterface $form, Ordre $ordre =null){
+        $builder = $form->getConfig()->getFormFactory()->createNamedBuilder(
+            'famille',
+            EntityType::class,
+            null,
+            [
+                'class' => 'AppBundle\Entity\Famille',
+                'placeholder'     => $ordre ? 'Sélectionnez la famille' : 'Sélectionnez l\'ordre',
+                'mapped'          => false,
+                'required'        => false,
+                'auto_initialize' => false,
+                'choices'         => $ordre ? $ordre->getFamilles() : []
+            ]
+        );
+        $builder->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) {
+                $form = $event->getForm();
+                $this->addEspeceField($form->getParent(), $form->getData());
+            }
+        );
+        $form->add($builder->getForm());
+    }
+
+
+    private function addEspeceField(FormInterface $form, Famille $famille = null)
+    {
+        $form->add('espece', EntityType::class, [
+            'class'       => 'AppBundle\Entity\Espece',
+            'placeholder' => $famille ? 'Sélectionnez l\'oiseau' : 'Sélectionnez votre département',
+            'choices'     => $famille ? $famille->getEspeces() : []
+        ]);
     }
     
     /**
