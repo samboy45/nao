6589b73c8718389c43fc9fabaddad30fd150c11f
Yann CLEMENT <biduletruck@gmail.com>
2017-05-31 17:43:03 +0200

    mise en place branch dashboard

--- src/NAO/UserBundle/Controller/DefaultController.php
+++ src/NAO/UserBundle/Controller/DefaultController.php
@@ -2,12 +2,55 @@
 
 namespace NAO\UserBundle\Controller;
 
+
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+
 
 class DefaultController extends Controller
 {
+
+
     public function indexAction()
     {
         return $this->render('UserBundle:Default:index.html.twig');
     }
+
+
+    public function listeDesUtilisateursAction() {
+        $userManager = $this->get('fos_user.user_manager');
+        $users = $userManager->findUsers();
+
+        return $this->render("Utilisateur/liste.html.twig", array('users' =>   $users));
+
+    }
+
+    public function trouverUnUtilisateursAction($id) {
+        $userManager = $this->get('fos_user.user_manager');
+        $user = $userManager->findUserBy(array('username' => $id));
+        return $user;
+
+    }
+
+    public function modifierUnUtilisateursAction($id) {
+        $userManager = $this->get('fos_user.user_manager');
+        $user = $this->trouverUnUtilisateursAction($id);
+        //$user->setEmail('cetemail@nexiste.pas');
+        $userManager->updateUser($user);
+
+    }
+
+    public function supprimerUnUtilisateur($id){
+        $userManager = $this->get('fos_user.user_manager');
+        $user = $this->trouverUnUtilisateursAction($id);
+        $userManager->deleteUser($user);
+    }
+
+
+
+
 }
--- src/NAO/UserBundle/Repository/UserRepository.php
+++ src/NAO/UserBundle/Repository/UserRepository.php
@@ -2,6 +2,7 @@
 
 namespace NAO\UserBundle\Repository;
 
+
 /**
  * UserRepository
  *
@@ -10,4 +11,15 @@ namespace NAO\UserBundle\Repository;
  */
 class UserRepository extends \Doctrine\ORM\EntityRepository
 {
+
+    public function findByRole($role)
+    {
+        $qb = $this->_em->createQueryBuilder();
+        $qb->select('u')
+            ->from($this->_entityName, 'u')
+            ->where('u.roles LIKE :roles')
+            ->setParameter('roles', '%"'.$role.'"%');
+
+        return $qb->getQuery()->getResult();
+    }
 }
