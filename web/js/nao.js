fde74a81f4c61770bd6552a37ef8e63d1ffc9a5d
FireKey <cyril.trehard@gmail.com>
2017-06-01 00:01:30 +0200

    ajout js global

---
+++ web/js/nao.js
@@ -0,0 +1,31 @@
+/**
+ * Created by firekey on 31/05/2017.
+ */
+
+$(function(){
+    var bodyElmt = $('body');
+    redimensionnerContenuNavigation();
+
+
+    /* ========================= GESTION EVENEMENTIELLE ========================= */
+
+    window.addEventListener('resize', function() {
+        redimensionnerContenuNavigation();
+    });
+
+
+    /* ========================= FONCTIONS ========================= */
+
+    function redimensionnerContenuNavigation() {
+        var boutonsContenuElmt = $('#bouton-nav>ul>li>a.btn');
+        if (bodyElmt.width() < 1040) {
+            boutonsContenuElmt.each(function () {
+                $(this).addClass('btn-xs');
+            });
+        } else {
+            boutonsContenuElmt.each(function () {
+                $(this).removeClass('btn-xs');
+            });
+        }
+    }
+});
