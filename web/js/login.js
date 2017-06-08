7aef415dc4a416174c7d639e7384cf3a690de589
FireKey <cyril.trehard@gmail.com>
2017-06-08 15:29:29 +0200

    mise en forme login

---
+++ web/js/login.js
@@ -0,0 +1,26 @@
+/**
+ * web/js/login.js
+ * Created by firekey on 08/06/2017.
+ */
+
+$(function(){
+    $('#Inscription main > div')
+        .removeAttr('id')
+        .addClass('bg-herbe-tropicale');
+    $('#Inscription input.btn')
+        .removeClass('btn-primary')
+        .addClass('btn-pin-glacial text-uppercase blanc');
+    retirer($('#Inscription meta'));
+    retirer($('#Inscription title'));
+    retirer($('#Inscription link'));
+    retirer($('#Inscription header'));
+
+
+    /* ========================= FONCTIONS ========================= */
+
+    function retirer(elmtTab){
+        elmtTab.each(function(){
+            $(this).remove();
+        })
+    }
+});
