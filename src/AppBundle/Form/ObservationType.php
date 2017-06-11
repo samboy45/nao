c7d340f42761abcddaca8aeb61e556e420350182
Yann CLEMENT <biduletruck@gmail.com>
2017-06-11 09:52:38 +0200

    correction orthographique

--- src/AppBundle/Form/ObservationType.php
+++ src/AppBundle/Form/ObservationType.php
@@ -78,7 +78,7 @@ class ObservationType extends AbstractType
             null,
             [
                 'class' => 'AppBundle\Entity\Famille',
-                'placeholder'     => $ordre ? 'Sélectionnez la famille' : 'Sélectionnez l\'ordre',
+                'placeholder'     => $ordre ? 'Sélectionnez la famille' : 'Sélectionnez l\'oiseau',
                 'mapped'          => false,
                 'required'        => false,
                 'auto_initialize' => false,
