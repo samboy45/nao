b05bc90d0e96c96dbb4dc00aa0ffe6df029e5871
FireKey <cyril.trehard@gmail.com>
2017-06-27 17:05:59 +0200

    utilisateurAction : ajout tableau user

--- src/NAO/ContactBundle/Controller/DefaultController.php
+++ src/NAO/ContactBundle/Controller/DefaultController.php
@@ -15,57 +15,42 @@ class DefaultController extends Controller
             'action' => $this->generateUrl('nao_contact_homepage'),
             'method' => 'POST'
         ));
-
-        if ($request->isMethod('POST')) {
-
+        if ($request->isMethod('POST')){
             $form->handleRequest($request);
-
-            if($form->isValid()){
-
+            if ($form->isValid()){
                 $recaptcha = new ReCaptcha($this->getParameter('recaptcha_key'));
                 $resp = $recaptcha->verify($request->request->get('g-recaptcha-response'), $request->getClientIp());
-
-                if (!$resp->isSuccess()) {
-
+                if (!$resp->isSuccess()){
                     $message = "Le reCAPTCHA n'a pas été entré correctement. Merci de réessayez.";
-
                     $this->get('session')->getFlashBag()->add('danger', $message);
+
                     return $this->redirectToRoute('nao_contact_homepage');
                 } else {
                     // envoi mail
-                    if($this->envoiEmail($form->getData())){
-
+                    if ($this->envoiEmail($form->getData())){
                         return $this->redirectToRoute('nao_contact_homepage');
                     }
                 }
-
-
             }
         }
-
-
 
         return $this->render('NAOContactBundle::contact.html.twig', array(
             'form' => $form->createView()
         ));
     }
 
-    private function envoiEmail($data){
+    private function envoiEmail($data)
+    {
         $ContactEmail = $this->getParameter('mailer_user'); //mettre email de contact
         $ContactMotDePasse = $this->getParameter('mailer_password'); // mettre mot de passe de l'adresse mail de contact
-
-
-        $transport = \Swift_SmtpTransport::newInstance( $this->getParameter('mailer_host'),$this->getParameter('mailer_port'),$this->getParameter('mailer_security'))
+        $transport = \Swift_SmtpTransport::newInstance($this->getParameter('mailer_host'),$this->getParameter('mailer_port'),$this->getParameter('mailer_security'))
             ->setUsername($ContactEmail)
             ->setPassword($ContactMotDePasse);
-
         $mailer = \Swift_Mailer::newInstance($transport);
 
         $message = \Swift_Message::newInstance("Nouveau contact depuis notre formulaire: ". $data["sujet"])
             ->setFrom(array($ContactEmail => "Message de ".$data["nom"]))
-            ->setTo(array(
-                $ContactEmail => $ContactEmail
-            ))
+            ->setTo(array($ContactEmail => $ContactEmail))
             ->setBody($data["message"]."<br>Email du contact :".$data["email"]);
 
         return $mailer->send($message);
