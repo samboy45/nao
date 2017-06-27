095d8bbe14bff4a925cdfc6a5a84302ddc025def
samboy45 <170145@supinfo.com>
2017-06-28 00:00:57 +0200

    modif mineur

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,8 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use FOS\UserBundle\Model\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class DefaultController extends Controller
 {
@@ -37,11 +41,45 @@ class DefaultController extends Controller
         return $this->render("mentions/cgu.html.twig");
     }
 
-    public function dashboardAction()
+    public function dashboardAction(Request $request)
     {
         $users = $this->get('fos_user.user_manager')->findUsers();
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+        $form = $this->createFormBuilder(null)->add('recherche', TextType::class, array('attr' => array('placeholder' => 'Recherche')))->getForm();
+        $form->handleRequest($request);
 
-        return $this->render("admin/utilisateurs/dashboard.html.twig", array('users' => $users));
+        if ($form->isSubmitted() AND $form->isValid()) {
+            $recherche = $form->getData() ;
+            $users = $this->getDoctrine()->getRepository('UserBundle:User')->filtrerUtilisateurs($recherche['recherche']);
+
+            return $this->render("admin/utilisateurs/dashboard.html.twig", array('users' => $users));
+
+        }
+
+        $form2 = $this->createFormBuilder(null)
+            ->add('roles', ChoiceType::class, array(
+                'choices' => array(
+                    'Filtrer par rôles' => null,
+                    'Tous' => null,
+                    'Particulier' => 'i:0',
+                    'Naturaliste' => 'ROLE_NATURALISTE',
+                    'Administrateur' => 'ROLE_ADMIN'
+                )
+            ))->getForm();
+        $form2->handleRequest($request);
+
+        if ($form2->isSubmitted() AND $form2->isValid()){
+            $role = $form2->getData();
+            $users = $this->getDoctrine()->getRepository('UserBundle:User')->filtrerParRole($role['roles']);
+
+            return $this->render("admin/utilisateurs/dashboard.html.twig", array('users' => $users));
+
+        }
+
+        return $this->render("admin/utilisateurs/dashboard.html.twig", array('users' => $users, 'user' => $user, 'form' => $form->createView() , 'form2' => $form2->createView()));
     }
 
     public function qsnAction()
